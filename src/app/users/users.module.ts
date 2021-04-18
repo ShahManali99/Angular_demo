df4ef04edc041c74e1479fa5012ddf257be69095
Manali <manali2899@gmail.com>
2021-04-18 11:29:44 +0530

    signup component added to users module

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login.component';
+import { SignupComponent } from './signup.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LoginComponent,
+    SignupComponent
   ]
 })
 export class UsersModule { }
